a328360c8a7fd8c8b9ec44793b981068c7ba4723
pedrim-hc <correa.phr@gmail.com>
2024-02-20 14:54:26 -0300

    pronto

--- dados.js
+++ dados.js
@@ -1,11 +1,22 @@
-let resposta = document.getElementById("resposta")
-let resposta2 = document.getElementById("resposta2")
+let resposta = document.getElementById("resp1")
+let resposta2 = document.getElementById("resp2")
 let gerar = document.getElementById("gerar")
 
+function aleatorio(num){
+    let valor = 0
+    valor = Math.floor( Math.random() * (num-1)+ 1)
+
+    return valor
+}
+
 gerar.addEventListener('click', ()=>{
+let quantidade = Number(document.getElementById('qnt').value)
+aleatorio(quantidade)
+let resultado = aleatorio(quantidade)
+console.log(resultado)
+resposta.innerHTML = `o valor gerado = ${resultado}` 
 
 
 
 
-    
 })
