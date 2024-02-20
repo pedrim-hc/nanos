fa7b0c72ef346bec09f1ee03a80cd0c494cc1167
pedrim-hc <correa.phr@gmail.com>
2024-02-20 14:09:08 -0300

    css pronto e começo js

---
+++ dados.js
@@ -0,0 +1,11 @@
+let resposta = document.getElementById("resposta")
+let resposta2 = document.getElementById("resposta2")
+let gerar = document.getElementById("gerar")
+
+gerar.addEventListener('click', ()=>{
+
+
+
+
+    
+})
